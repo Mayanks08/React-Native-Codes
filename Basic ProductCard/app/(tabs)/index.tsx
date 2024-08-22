2aa21b1a1625d9e5380d1ae7fe98fcf4d9082a9f
Mayank saini <mayanksainikht@gmail.com>
2024-08-22 19:03:37 +0530

    ProductCart with search feature

--- Basic ProductCard/app/(tabs)/index.tsx
+++ Basic ProductCard/app/(tabs)/index.tsx
@@ -38,7 +38,7 @@ export default function App() {
 const styles = StyleSheet.create({
 
   container: {
-    backgroundColor:'#B9B4C7',
+    
     
   },
   itemContainer:{
--- Basic ProductCard/components/ProductList.js
+++ Basic ProductCard/components/ProductList.js
@@ -1,20 +1,37 @@
 import {FlatList, View,StyleSheet} from "react-native"
 import ProductCard from"./ProductCard"
+import SearchBar from "./searchBar"
+import { useState } from "react"
 
 const ProductList = ({products}) => {
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const filteredProducts = products.filter(product => 
+        product.name && product.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return ( 
+        <View style={styles.searchb}>
+        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         <FlatList 
-        data={products} 
+        data={filteredProducts} 
         renderItem={({item}) => (
-            <ProductCard name={item.name} price={item.price} descriptions={item.descriptions} />
+            <ProductCard 
+            name={item.name} 
+            price={item.price} 
+            descriptions={item.descriptions} />
         )}
-        keyExtractor={(item) => item.id}
+        keyExtractor={(item) => item.id.toString()}
 
         />
+        </View>
      );
      
 }
 export default ProductList;
 const styles = StyleSheet.create({
+    searchb: {
+       
+    }
     
 })
